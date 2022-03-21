1f6ca8fb20d929e1bef80d8991dcca598f531d3b
Sass <dudu_sass12@hotmail.com>
2022-03-21 19:13:44 -0300

    feat authorization users and byId

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const auth = require('../auth/auth');
 const router = express.Router();
 
 const { User } = require('../models');
-const { verifyUser, validateEmail } = require('../services/userServices.js');
+const { verifyUser, validateEmail, findById } = require('../services/userServices.js');
 
 router.post('/', async (req, res) => {
   try {
@@ -15,6 +15,35 @@ router.post('/', async (req, res) => {
     await User.create({ displayName, email, password, image });
     const token = auth.genToken(email);
     return res.status(201).json({ token });
+  } catch (error) {
+    return res.status(error.status).json({ message: error.message });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const { authorization } = req.headers;
+    await auth.verifyToken(authorization);
+
+    const users = await User.findAll({
+      attributes: { exclude: ['updatedAt', 'createdAt'] },
+    });
+
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(error.status).json({ message: error.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { authorization } = req.headers;
+    await auth.verifyToken(authorization);
+
+    const { id } = req.params;
+    const getUserByid = await findById(id);
+
+    return res.status(200).json(getUserByid);
   } catch (error) {
     return res.status(error.status).json({ message: error.message });
   }
